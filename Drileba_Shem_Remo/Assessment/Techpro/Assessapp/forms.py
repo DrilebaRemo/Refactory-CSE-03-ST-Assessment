1e9c215e66c3fa1a0683ac7b2d898880192767cb
Shem Remo <shemremo@gmail.com>
2024-05-17 15:39:27 +0300

    changed css

--- Drileba_Shem_Remo/Assessment/Techpro/Assessapp/forms.py
+++ Drileba_Shem_Remo/Assessment/Techpro/Assessapp/forms.py
@@ -10,10 +10,11 @@ class RegForm(forms.ModelForm):
         widgets = {
             'first_name':forms.TextInput(attrs={ 'required':'required'}),
             'last_name':forms.TextInput(attrs={ 'required':'required'}),
-            'course':forms.Select(attrs={'class':'form-contro', 'required':'required'}),
-            'entry_scheme':forms.Select(attrs={'class':'form-contro', 'required':'required'}),
-            'intake':forms.Select(attrs={'class':'form-contro', 'required':'required'}),
-            'sponsorship':forms.Select(attrs={'class':'form-contro', 'required':'required'}),
+            'course':forms.Select(attrs={ 'required':'required'}),
+            'entry_scheme':forms.Select(attrs={ 'required':'required'}),
+            'intake':forms.Select(attrs={ 'required':'required'}),
+            'sponsorship':forms.Select(attrs={ 'required':'required'}),
+            'gender':forms.RadioSelect(attrs={ 'required':'required'}),
             'date_of_birth':forms.DateInput(attrs={ 'required':'required'}),
             'residence':forms.TextInput(attrs={ 'required':'required'}),
         }
--- Drileba_Shem_Remo/Assessment/Techpro/Assessapp/views.py
+++ Drileba_Shem_Remo/Assessment/Techpro/Assessapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import *
 from .forms import *
+from Assessapp import views as auth_views
 
 # Create your views here.
 def index (request):
@@ -11,6 +12,8 @@ def index (request):
             form.save()
             messages.success(request, 'Form has been successfully submitted!')
             return redirect('index')
+        else:
+            messages.error(request, 'Form has some invalid fields!')
     else:
         form = RegForm()
     return render(request, 'Assessapp/index.html', {'form': form})
